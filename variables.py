b75d5c80fe4783eb08598cb4b6eef7fb47666b5d
Stark-kk <79964027+Stark-kk@users.noreply.github.com>
2021-08-07 20:33:33 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+a=1971
+b=1984
+c=2002
+d=b-a
+e=c-a
+print(d,\nb)
+#Rob's age is closer to the Unix's
+
+
+z=1
+x=z+1
+y=z*2
+print(x==2 and y==2)#True
+print(x<=1 or y>=2)#True
+print(x!=2 or y<!)#False
