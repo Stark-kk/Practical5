bf72c994f411c02b4958e7e48ade5bcd5810c07d
Stark-kk <79964027+Stark-kk@users.noreply.github.com>
2021-08-07 21:30:53 +0800

    Update mystery_code.py

--- mystery_code.py
+++ mystery_code.py
@@ -1,21 +1,20 @@
+-- mystery_code.py --
 # What does this piece of code do?
-# Answer: The code want to find a number<=50 from 1 to 99
-
+# Answer:
 # Import libraries
-# randint allows drawing a random number, 
+# randint allows drawing a random number,
 # e.g. randint(1,5) draws a number between 1 and 5
 from random import randint
-
 # ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
-
-
 p=False
 while p==False:
-	p = True
-	n = randint(1,100)
-	if n > 50:
-		p = False
+ p=True
+ n = randint(1,100)#choose a number from 1 to 99
+ u = ceil(n**(0.5))#u=the next higher interger of n**0.5
+ for i in range(2,u+1):
+ if n%i == 0:
+ p=False
 
 print(n)
